c41346d3df4821cfa9c1a25240fd570bf8d7505f
devJane096 <je5.lee@onul-hoi.com>
2021-09-12 21:53:17 +0900

    주석문 제거

--- src/test/java/com/depromeet/breadmapbackend/BreadMapBackendApplicationTests.java
+++ src/test/java/com/depromeet/breadmapbackend/BreadMapBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.depromeet.breadmapbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class BreadMapBackendApplicationTests {
 
     @Test
