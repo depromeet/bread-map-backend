2e1be612b93e00f1078148a029c913b206eb88ca
devJane096 <je5.lee@onul-hoi.com>
2021-09-12 21:36:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/depromeet/breadmapbackend/BreadMapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.breadmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadMapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreadMapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/breadmapbackend/BreadMapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.breadmapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class BreadMapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
