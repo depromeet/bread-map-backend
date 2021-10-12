f0291ede59a978ba215ca913820b81812774614c
earth-h <earth.h.183@gmail.com>
2021-10-13 00:08:45 +0900

    [#26] CORS 체크를 위한 WebConfig.java 내용 원복

--- src/main/java/com/depromeet/breadmapbackend/common/config/WebConfig.java
+++ src/main/java/com/depromeet/breadmapbackend/common/config/WebConfig.java
@@ -1,17 +1,17 @@
-//package com.depromeet.breadmapbackend.common.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods("GET", "POST", "OPTIONS");
-//                //.allowedOrigins("*");
-//    }
-//
-//}
+package com.depromeet.breadmapbackend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "OPTIONS");
+                //.allowedOrigins("*");
+    }
+
+}
