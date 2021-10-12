4aef1f6b8ca2261a7e8c428d66cf36db1777b170
earth-h <earth.h.183@gmail.com>
2021-10-12 23:32:34 +0900

    [#26] CORS 체크를 위해 임시로 WebConfig.java 파일 주석 처리(nginx에서 CORS 이슈 해결하기 위함)

--- src/main/java/com/depromeet/breadmapbackend/common/config/WebConfig.java
+++ src/main/java/com/depromeet/breadmapbackend/common/config/WebConfig.java
@@ -7,11 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+/*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedOrigins("*");
     }
+*/
 
 }
