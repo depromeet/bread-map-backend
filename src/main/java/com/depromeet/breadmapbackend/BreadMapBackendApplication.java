dad8909d34a7a6888162c143a531bcc8f4b6be4b
earth-h <earth.h.183@gmail.com>
2021-10-13 17:16:07 +0900

    [#26] TIMEZONE 오류 체크 (Bean 생명주기 통한 timezone 설정) => 기존 db 날리기 위해 ddl-auto: create 변경

--- src/main/java/com/depromeet/breadmapbackend/BreadMapBackendApplication.java
+++ src/main/java/com/depromeet/breadmapbackend/BreadMapBackendApplication.java
@@ -6,11 +6,19 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EntityScan("com.depromeet.breadmapbackend")
 @EnableJpaRepositories("com.depromeet.breadmapbackend")
 public class BreadMapBackendApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BreadMapBackendApplication.class, args);
