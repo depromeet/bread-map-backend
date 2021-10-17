1a27c4ad7db38c6f4f178a0fc92ddcae6e5f8243
2haein <eehaein2@gmail.com>
2021-10-17 22:33:40 +0900

    [#51] ReviewsListResponse 수정

--- src/main/java/com/depromeet/breadmapbackend/reviews/dto/ReviewsListResponse.java
+++ src/main/java/com/depromeet/breadmapbackend/reviews/dto/ReviewsListResponse.java
@@ -14,9 +14,8 @@ public class ReviewsListResponse {
 
     private Long reviewId;
     private Long bakeryId;
+    private Long memberId;
     private String contents;
     private Integer rating;
-    private List<FilterType> filterTypeList;
-    private List<String> filterNameList;
     private List<String> reviewImgPathList;
 }
