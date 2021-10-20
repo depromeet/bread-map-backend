ac7979b8dcaff8a0cb2173b58e6226ffcb16c270
earth-h <earth.h.183@gmail.com>
2021-10-20 17:09:46 +0900

    [#56] ImageType 변경(exterior, interior 대신 bakery 추가)

--- src/main/java/com/depromeet/breadmapbackend/common/enumerate/ImageType.java
+++ src/main/java/com/depromeet/breadmapbackend/common/enumerate/ImageType.java
@@ -4,9 +4,6 @@ package com.depromeet.breadmapbackend.common.enumerate;
     빵집 외부 이미지 사진, 빵집 내부이미지 사진, 리뷰 이미지 enum type
  */
 public enum ImageType {
-    EXTERIOR,
-
-    INTERIOR,
-
+    BAKERY,
     REVIEW;
 }
