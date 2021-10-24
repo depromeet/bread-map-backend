c5320b5257e009bf77f8d5f0de4ef9f9f9489b20
Jane096 <jes7077@naver.com>
2021-10-24 21:49:18 +0900

    [develop] 불필요한 import 제거

--- src/main/java/com/depromeet/breadmapbackend/bakeries/repository/BakeriesBreadCategoriesMapQuerydslRepository.java
+++ src/main/java/com/depromeet/breadmapbackend/bakeries/repository/BakeriesBreadCategoriesMapQuerydslRepository.java
@@ -1,9 +1,5 @@
 package com.depromeet.breadmapbackend.bakeries.repository;
 
-import com.depromeet.breadmapbackend.bakeries.domain.BakeriesBreadCategoriesMap;
-import com.depromeet.breadmapbackend.bakeries.domain.QBakeriesBreadCategoriesMap;
-import com.depromeet.breadmapbackend.bakeries.dto.BakeryInfoResponse;
-import com.depromeet.breadmapbackend.common.enumerate.BreadCategoryType;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.depromeet.breadmapbackend.bakeries.domain.QBakeries.bakeries;
 import static com.depromeet.breadmapbackend.bakeries.domain.QBakeriesBreadCategoriesMap.bakeriesBreadCategoriesMap;
-import static com.depromeet.breadmapbackend.flags.domain.QFlags.flags;
-import static com.depromeet.breadmapbackend.reviews.domain.QBakeryReviews.bakeryReviews;
-import static com.depromeet.breadmapbackend.reviews.domain.QMenuReviews.menuReviews;
 
 @Repository
 @RequiredArgsConstructor
