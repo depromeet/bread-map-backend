2cdfad267c17fe903a3a1144b977bd2320739af9
Jane096 <jes7077@naver.com>
2021-11-01 01:00:28 +0900

    [#86] BakeriesService 작성중

--- src/test/java/com/depromeet/breadmapbackend/BakeriesServiceTest.java
+++ src/test/java/com/depromeet/breadmapbackend/BakeriesServiceTest.java
@@ -1,17 +1,31 @@
 package com.depromeet.breadmapbackend;
 
 import com.depromeet.breadmapbackend.auth.service.AuthService;
+import com.depromeet.breadmapbackend.bakeries.domain.Bakeries;
+import com.depromeet.breadmapbackend.bakeries.dto.CreateBakeryRequest;
+import com.depromeet.breadmapbackend.bakeries.repository.BakeriesQuerydslRepository;
+import com.depromeet.breadmapbackend.bakeries.repository.BakeriesRepository;
 import com.depromeet.breadmapbackend.bakeries.service.BakeriesService;
 import com.depromeet.breadmapbackend.members.domain.Members;
 import com.depromeet.breadmapbackend.members.repository.MemberQuerydslRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.depromeet.breadmapbackend.members.repository.MemberRepository;
+import org.javaunit.autoparams.AutoSource;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BakeriesServiceTest {
@@ -20,22 +34,55 @@ public class BakeriesServiceTest {
     private BakeriesService bakeriesService;
 
     @Mock
-    private MemberQuerydslRepository memberQuerydslRepository;
+    private MemberRepository memberRepository;
+
+    @Mock
+    private BakeriesQuerydslRepository bakeriesQuerydslRepository;
+
+    @Mock
+    private BakeriesRepository bakeriesRepository;
 
     @Mock
     private AuthService authService;
 
-    private static final String TOKEN = "1234zbdsdfdf";
-    private static final Long BAKERY_ID = 1L;
+    private static final String TOKEN = "testToken";
     private static final Long MEMBER_ID = 1L;
 
-    @Test
-    @DisplayName("테스트")
-    void test() {
-        //when
-        Members member = memberQuerydslRepository.findByIdAndBakeryId(MEMBER_ID, BAKERY_ID);
+    @ParameterizedTest
+    @AutoSource
+    void 이미_존재하는_빵집일_경우_ResponseStatusException_예외가_발생한다(CreateBakeryRequest createBakeryRequest, Bakeries bakeries) {
+        when(bakeriesQuerydslRepository.isBakeryExisted(createBakeryRequest.getLatitude(), createBakeryRequest.getLongitude())).thenReturn(true);
 
-        //then
-        //assertThat(member.getId(), )
+        Exception exception = assertThrows(ResponseStatusException.class, () -> bakeriesService.createBakery(TOKEN, createBakeryRequest));
+        String exceptedMessage = "이미 등록 된 빵집입니다.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(exceptedMessage));
+        then(bakeriesRepository).should(never()).save(bakeries);
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void 신규_빵집일_경우_사용자조회_후_데이터를_저장한다(CreateBakeryRequest createBakeryRequest, Members members) {
+        when(bakeriesQuerydslRepository.isBakeryExisted(createBakeryRequest.getLatitude(), createBakeryRequest.getLongitude())).thenReturn(false);
+        given(authService.getMemberId(TOKEN)).willReturn(MEMBER_ID);
+        when(memberRepository.findById(MEMBER_ID)).thenReturn(Optional.ofNullable(members));
+
+        Bakeries data = Bakeries.builder()
+                .name(createBakeryRequest.getBakeryName())
+                .latitude(createBakeryRequest.getLatitude())
+                .longitude(createBakeryRequest.getLongitude())
+                .members(members)
+                .telNumber(createBakeryRequest.getTelNumber())
+                .address(createBakeryRequest.getAddress())
+                .businessHour(createBakeryRequest.getBusinessHour())
+                .basicInfoList(createBakeryRequest.getBasicInfoList())
+                .websiteUrlList(createBakeryRequest.getWebsiteUrlList())
+                .imgPath(createBakeryRequest.getImgPathList())
+                .build();
+
+        //then(bakeriesRepository).should().save(data);
+        bakeriesService.createBakery(TOKEN, createBakeryRequest);
+        //verify(bakeriesRepository).save(data);
     }
 }
