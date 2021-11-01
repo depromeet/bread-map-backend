f01de444ed1158faffa8fc8f5543a4412d0ae4c3
earth-h <earth.h.183@gmail.com>
2021-11-01 20:15:31 +0900

    [#85] 메뉴서비스 관련 예외 테스트 추가(ParameterizedTest 사용)

---
+++ src/test/java/com/depromeet/breadmapbackend/earth/MenuReviewsServiceTest.java
@@ -0,0 +1,30 @@
+package com.depromeet.breadmapbackend.earth;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MenuReviewsServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void deleteMenuReview() {
+    }
+
+    @Test
+    void createMenuReviewList() {
+    }
+
+    @Test
+    void getMenuReviewList() {
+    }
+}
