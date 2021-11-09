b4d41390c4408d1a0baec0885ecf062108b18eb2
earth-h <earth.h.183@gmail.com>
2021-11-10 00:57:31 +0900

    [#94] #81 PR 관련 rating 빠진 부분 추가 작업 및 MenuReviewResponse 내 @Builder 중복 제거

--- src/main/java/com/depromeet/breadmapbackend/reviews/repository/MenuReviewQuerydslRepository.java
+++ src/main/java/com/depromeet/breadmapbackend/reviews/repository/MenuReviewQuerydslRepository.java
@@ -3,6 +3,7 @@ package com.depromeet.breadmapbackend.reviews.repository;
 import com.depromeet.breadmapbackend.bakeries.dto.BakeryMenuResponse;
 import com.depromeet.breadmapbackend.reviews.domain.MenuReviews;
 import com.depromeet.breadmapbackend.reviews.dto.MenuReviewResponse;
+import com.depromeet.breadmapbackend.reviews.dto.SimpleMenuReviewResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,24 @@ public class MenuReviewQuerydslRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
+    /**
+     * 생성날짜 기준(최신순) MenuReviews 리스트 반환(contents만 반환하는 simple형)
+     */
+    public List<SimpleMenuReviewResponse> findSimpleMenuReviewListByBakeryId(Long bakeryId, Long offset, Long limit) {
+        return jpaQueryFactory
+                .select(Projections.fields(SimpleMenuReviewResponse.class,
+                        menuReviews.contents))
+                .from(menuReviews)
+                .where(menuReviews.bakeries.id.eq(bakeryId))
+                .orderBy(menuReviews.createdDateTime.desc())
+                .offset(offset)
+                .limit(limit)
+                .fetch();
+    }
+
+    /**
+     * 생성날짜 기준(최신순) MenuReviews 리스트 반환(offset/limit 직접 사용)
+     */
     public List<MenuReviewResponse> findMenuReviewListByBakeryId(Long bakeryId, Long offset, Long limit) {
         return jpaQueryFactory
                 .select(Projections.fields(MenuReviewResponse.class,
@@ -31,8 +50,8 @@ public class MenuReviewQuerydslRepository {
                         menuReviews.members.id.as("memberId"),
                         menuReviews.menus.id.as("menuId"),
                         menuReviews.imgPath.as("imgPathList"),
-                        menuReviews.contents,
-                        menuReviews.rating,
+                        menuReviews.contents.as("contents"),
+                        menuReviews.rating.as("rating"),
                         menuReviews.menus.name.as("menuName"),
                         menuReviews.members.name.as("memberName"),
                         menuReviews.menus.breadCategories.id.as("breadCategoryId"),
@@ -46,6 +65,44 @@ public class MenuReviewQuerydslRepository {
                 .fetch();
     }
 
+    /**
+     * 생성날짜 기준(최신순) MenuReviews 리스트 반환(pageable 사용)
+     */
+    public Slice<MenuReviewResponse> findMenuReviewPageableByBakeryId(Long bakeryId, Pageable pageable) {
+        List<MenuReviewResponse> menuReviewResponsesList = jpaQueryFactory
+                .select(Projections.fields(MenuReviewResponse.class,
+                        menuReviews.id.as("menuReviewId"),
+                        menuReviews.members.id.as("memberId"),
+                        menuReviews.menus.breadCategories.id.as("breadCategoryId"),
+                        menuReviews.members.name.as("memberName"),
+                        menuReviews.menus.name.as("menuName"),
+                        menuReviews.menus.id.as("menuId"),
+                        menuReviews.imgPath.as("imgPathList"),
+                        menuReviews.contents.as("contents"),
+                        menuReviews.rating.as("rating"),
+                        menuReviews.lastModifiedDateTime.as("lastModifiedDateTime")))
+                .from(menuReviews)
+                .where(menuReviews.bakeries.id.eq(bakeryId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        List<MenuReviewResponse> content = new ArrayList<>();
+        for (MenuReviewResponse menuReview: menuReviewResponsesList) {
+            content.add(new MenuReviewResponse(menuReview));
+        }
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            content.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+        return new SliceImpl<>(content, pageable, hasNext);
+    }
+
+    /**
+     * menuReview 기준(리뷰 많은순) BakeryMenu 리스트 반환(offset/limit 직접 사용)
+     */
     public List<BakeryMenuResponse> findBakeryMenuListByBakeryId(Long bakeryId, Long offset, Long limit) {
         return jpaQueryFactory
                 .select(Projections.fields(BakeryMenuResponse.class,
@@ -65,6 +122,9 @@ public class MenuReviewQuerydslRepository {
                 .fetch();
     }
 
+    /**
+     * menuReview 기준(리뷰 많은순) BakeryMenu 리스트 반환(pageable 사용)
+     */
     public Slice<BakeryMenuResponse> findBakeryMenuPageableByBakeryId(Long bakeryId, Pageable pageable) {
         List<BakeryMenuResponse> bakeryMenuResponseList = jpaQueryFactory
                 .select(Projections.fields(BakeryMenuResponse.class,
@@ -105,34 +165,5 @@ public class MenuReviewQuerydslRepository {
                 .fetchOne();
     }
 
-    public Slice<MenuReviewResponse> findMenuReviewPageableByBakeryId(Long bakeryId, Pageable pageable) {
-        List<MenuReviewResponse> menuReviewResponsesList = jpaQueryFactory
-                .select(Projections.fields(MenuReviewResponse.class,
-                        menuReviews.id.as("menuReviewId"),
-                        menuReviews.members.id.as("memberId"),
-                        menuReviews.menus.breadCategories.id.as("breadCategoryId"),
-                        menuReviews.members.name.as("memberName"),
-                        menuReviews.menus.name.as("menuName"),
-                        menuReviews.menus.id.as("menuId"),
-                        menuReviews.imgPath.as("imgPathList"),
-                        menuReviews.contents.as("contents"),
-                        menuReviews.lastModifiedDateTime.as("lastModifiedDateTime")))
-                .from(menuReviews)
-                .where(menuReviews.bakeries.id.eq(bakeryId))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize() + 1)
-                .fetch();
 
-        List<MenuReviewResponse> content = new ArrayList<>();
-        for (MenuReviewResponse menuReview: menuReviewResponsesList) {
-            content.add(new MenuReviewResponse(menuReview));
-        }
-
-        boolean hasNext = false;
-        if (content.size() > pageable.getPageSize()) {
-            content.remove(pageable.getPageSize());
-            hasNext = true;
-        }
-        return new SliceImpl<>(content, pageable, hasNext);
-    }
 }
