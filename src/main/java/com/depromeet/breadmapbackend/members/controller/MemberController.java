a8be826f6a431b475789be26c2e0e061df804847
Jane096 <jes7077@naver.com>
2021-11-10 22:18:09 +0900

    [#93] 스웨거 operation 추가

--- src/main/java/com/depromeet/breadmapbackend/members/controller/MemberController.java
+++ src/main/java/com/depromeet/breadmapbackend/members/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.depromeet.breadmapbackend.auth.jwt.JwtHeaderUtil;
 import com.depromeet.breadmapbackend.common.dto.ApiResponse;
 import com.depromeet.breadmapbackend.members.dto.UserInfoResponse;
 import com.depromeet.breadmapbackend.members.service.MembersService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,12 @@ public class MemberController {
 
     private final MembersService membersService;
 
+    /**
+     * 유저 정보 조회
+     * @param HttpServletRequest
+     * @return ResponseEntity<UserInfoResponse>
+     */
+    @ApiOperation(value = "로그인한 유저의 정보 조회", notes = "로그인한 유저의 정보 조회")
     @GetMapping("/info")
     public ResponseEntity<UserInfoResponse> getUserInfo(HttpServletRequest request) {
         String token = JwtHeaderUtil.getAccessToken(request);
