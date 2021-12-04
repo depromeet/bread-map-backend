a3a06926ec6f2ae81d359cb612cc2cd657a41350
Jane096 <jes7077@naver.com>
2021-12-04 17:05:20 +0900

    [#124] 이미지 리사이즈 추가

--- src/main/java/com/depromeet/breadmapbackend/common/service/AwsS3Service.java
+++ src/main/java/com/depromeet/breadmapbackend/common/service/AwsS3Service.java
@@ -6,12 +6,18 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import marvin.image.MarvinImage;
+import org.marvinproject.image.transform.scale.Scale;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -32,11 +38,12 @@ public class AwsS3Service {
 
         multipartFile.forEach(file -> {
             String fileName = createFileName(file.getOriginalFilename());
+            MultipartFile resizedFile = resizeImage(fileName, file, 768, 768);
             ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentLength(resizedFile.getSize());
             objectMetadata.setContentType(file.getContentType());
 
-            try(InputStream inputStream = file.getInputStream()) {
+            try(InputStream inputStream = resizedFile.getInputStream()) {
                 amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
                         .withCannedAcl(CannedAccessControlList.PublicRead));
             } catch(IOException e) {
@@ -62,6 +69,28 @@ public class AwsS3Service {
             return fileName.substring(fileName.lastIndexOf("."));
         } catch (StringIndexOutOfBoundsException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
+        }
+    }
+
+    MultipartFile resizeImage(String fileName, MultipartFile originalImage, int targetWidth, int targetHeight) {
+        try {
+            BufferedImage image = ImageIO.read(originalImage.getInputStream());
+            MarvinImage imageMarvin = new MarvinImage(image);
+            Scale scale = new Scale();
+            scale.load();
+            scale.setAttribute("newWidth", targetWidth);
+            scale.setAttribute("newHeight", targetHeight);
+            scale.process(imageMarvin.clone(), imageMarvin, null, null, false);
+
+            BufferedImage imageNoAlpha = imageMarvin.getBufferedImageNoAlpha();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(imageNoAlpha, "jpg", baos);
+            baos.flush();
+
+            return new MockMultipartFile(fileName, baos.toByteArray());
+
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이즈에 실패했습니다.");
         }
     }
 }
